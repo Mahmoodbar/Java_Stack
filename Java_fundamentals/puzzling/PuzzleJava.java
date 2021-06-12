a23e58a696c09c66411ee1dacdc02f478095d3b3
mahmood Barakat <mahmood.barakat.2011@gmail.com>
2021-06-12 17:02:21 +0300

    java

---
+++ Java_fundamentals/puzzling/PuzzleJava.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Arrays;
+import java.util.Random;
+
+
+
+
+
+public class PuzzleJava{
+
+    public static void main(String[] args){
+
+
+
+
+        ////////////////1
+      
+    // int [] myArray = {3,5,1,2,7,9,8,13,25,32};
+    //   int sum = 0;
+      
+    
+
+    //     for (int i=0 ; i<myArray.length; i++){
+    //             sum = sum + myArray[i] ;
+    //             if ( myArray[i] > 10 ){
+    //                 System.out.println(myArray[i]);
+    //             }
+    //     }
+    //     System.out.println(sum);
+    
+
+
+
+
+//////////////////2
+    // ArrayList<String> strArray = new ArrayList();
+    // String [] myArray = {"nancy","jinchi","fujibayashi","momochi","ishikawa"};
+
+
+    // for ( int i = 0 ; i<myArray.length ; i++){
+    //       if ( myArray[i].length()>5){
+    //                     System.out.println(myArray[i]);
+    //                 }
+    //     strArray.add(myArray[i]);
+
+    // }
+    // Collections.shuffle(strArray);
+    // System.out.println(strArray);
+
+
+
+
+    ////////////////3
+
+    // ArrayList<Character> charArray = new ArrayList();
+
+    // for (char i='a' ; i<='z' ; i++){
+    //     charArray.add(i);
+
+    // }
+
+    //     System.out.println(charArray);
+    //     Collections.shuffle(charArray);
+    //     System.out.println(charArray.get(0));
+    //     System.out.println(charArray.get(charArray.size()-1));
+
+    //     ArrayList<Character> vowelArray = new ArrayList();
+
+    //     vowelArray.add('i');
+    //     vowelArray.add('e');
+    //     vowelArray.add('u');
+    //     vowelArray.add('a');
+    //     vowelArray.add('o');
+
+    //     if (vowelArray.contains(charArray.get(0))){
+    //        System.out.println("ITS A VOWEL");
+        
+    //     }
+
+
+
+
+    ///////////4+5
+
+   
+    
+        // ArrayList<Integer> numbers = new ArrayList<Integer>();
+        // Random r = new Random();
+        // for (int i = 0; i < 10; i++) {
+        
+        //     numbers.add(r.nextInt(45) + 55);
+        // }
+        
+        // Collections.sort(numbers);
+        // System.out.println(String.format("The Minimum is %d",numbers.get(0)));
+        // System.out.println(String.format("The Maximum is %d",numbers.get(9)));
+            
+    
+
+    
+        
+
+
+ }
+   
+}   
