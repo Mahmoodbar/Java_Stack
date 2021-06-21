2a064c8e4c6bf0469c7c93467312d3bf38690054
mahmood Barakat <mahmood.barakat.2011@gmail.com>
2021-06-21 18:14:23 +0300

    java

---
+++ Java_spring/UpdateDelete/src/test/java/com/section/updatedelete/UpdateDeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section.updatedelete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdateDeleteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
